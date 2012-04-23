c625e68d1a6d7738fafa857367ed6c8530e632c6
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-23 12:00:41 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AkiinyoBlog::Application.config.secret_token = '5e3eed5c1f6bdd6d14e71524a0f731dedb5f69f4b81a835cb7ceed8bfc4c7898392eb2c2921dbf61154acdc82764ba441f23ebba7ffa93dc9d4f9c117fc6fe93'
