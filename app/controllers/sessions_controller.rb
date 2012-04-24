80ae1e28474bae2e8acc560661fe2dc104e637c4
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-24 11:21:25 +0900

    fix session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
       sign_in user
       redirect_back_or user
     else
