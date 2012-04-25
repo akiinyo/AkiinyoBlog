97f3805b7c85c66d9200bb7692fdee0fd799f9cb
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-25 17:24:53 +0900

    Postを編集と削除できるようにした

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -15,4 +15,23 @@ class Users::PostsController < ApplicationController
       redirect_to user_posts_url(current_user)
     end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      redirect_to user_post_url(current_user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_posts_url
+  end
 end
