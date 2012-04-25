497ea8e405efcffc08e2ac89bc7ed34affe7fd8e
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-25 20:49:31 +0900

    localeやTimezoneをいちいち気にしなくていいようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :japanese!
   include SessionsHelper
+
+  def japanese!
+    I18n.locale = :ja
+    Time.zone = 'Asia/Tokyo'
+    response['Content-Language'] = 'content="ja"'
+  end
 end
