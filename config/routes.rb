1691291ab65fe04330ac1cf6b7080efe8630533b
Tagaki Aki <a-tagaki@esm.co.jp>
2012-04-27 13:49:21 +0900

    onlyをつけ忘れていた

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ AkiinyoBlog::Application.routes.draw do
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
 
-  resources :users do
+  resources :users, only: [:new, :create, :show] do
     resources :posts, controller: 'users/posts' do
       resources :comments, controller: 'users/posts/comments'
     end
     resources :profiles, controller: 'users/profiles'
-    resources :buttons, controller: 'users/buttons' do
+    resources :buttons, controller: 'users/buttons', only: [:new, :create, :destroy] do
       resources :puts, controller: 'users/buttons/puts', only: [:create]
     end
   end
