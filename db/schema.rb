b5c55f95e5d1a492f3447d579a98ef7220f795c6
Tagaki Aki <a-tagaki@esm.co.jp>
2012-05-16 19:38:47 +0900

    Authenticationモデルを作った
    後からfacebookとかgithubとか足せるように

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120426101719) do
+ActiveRecord::Schema.define(:version => 20120516050738) do
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "buttons", :force => true do |t|
     t.integer  "user_id",    :null => false
