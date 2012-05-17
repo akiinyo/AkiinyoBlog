0c7d5fd30afdcf05c7df9f82c1cb483fbf48820d
Tagaki Aki <a-tagaki@esm.co.jp>
2012-05-17 19:32:17 +0900

    間違えてた

--- db/migrate/20120517014458_remove_email_and_password_digest_to_users.rb
+++ db/migrate/20120517014458_remove_email_and_password_digest_to_users.rb
@@ -1,11 +1,9 @@
 class RemoveEmailAndPasswordDigestToUsers < ActiveRecord::Migration
   def up
     remove_column :users, :email
-    remove_column :users, :passwaord_digest
   end
 
   def down
-    add_column :users, :passwaord_digest, :string
     add_column :users, :email, :string
   end
 end
