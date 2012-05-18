fcec0e2ae52a9e8eb761fbcab8a6bd152ea5db06
Tagaki Aki <a-tagaki@esm.co.jp>
2012-05-18 17:55:14 +0900

    パスワードとユーザ名でもログインできるように修復

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if request.env["omniauth.auth"]
+      create_by_omniauth_twitter
+    else
+      create_by_password
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
+  private
+
+  def create_by_omniauth_twitter
     auth = request.env["omniauth.auth"]
     if signed_in?
       Authentication.connect(current_user, auth["provider"], auth["uid"], auth["info"]["image"])
@@ -11,11 +26,20 @@ class SessionsController < ApplicationController
       authentication = Authentication.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Authentication.create_with_omniauth(auth)
       sign_in authentication.user
     end
-    redirect_to user_path(current_user), notice: "ログインしました。"
+    sucess!
   end
 
-  def destroy
-    sign_out
-    redirect_to root_path
+  def create_by_password
+    user = User.find_by_name(params[:name])
+    if user && user.authenticate(params[:password])
+      sign_in user
+      sucess!
+    else
+      redirect_to root_path, notice: 'ログインに失敗しました。'
+    end
+  end
+
+  def sucess!
+    redirect_to user_path(current_user), notice: "ログインしました。"
   end
 end
