1b0ae5650cd518714ed693252e4c74f4079a410e
Tagaki Aki <a-tagaki@esm.co.jp>
2012-05-30 17:08:08 +0900

    コメントを受け付けるかどうかの判定メソッドを作った

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,11 @@ class User < ActiveRecord::Base
     password_digest.nil? || password_digest == DUMMY_PASSWORD
   end
 
+  def receive_comment?
+    return true unless profile
+    profile.comment
+  end
+
   private
 
   def create_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,25 @@ require 'spec_helper'
 
 describe User do
 
-  describe 'コメントを受け取るかどうかを選択する' do
+  describe 'コメントを受け付けるかどうかを選択する' do
     let!(:user) { FactoryGirl.create(:user) }
 
-    context '受け取る' do
+    context '受け付ける' do
       before do
         user.build_profile(blog_title: 'タイトル', message: 'よろしく', comment: true)
         user.save!
       end
 
-      specify { user.profile.comment.should be_true }
+      specify { user.receive_comment? ==  be_true }
+    end
+
+    context '受け付けない' do
+      before do
+        user.build_profile(blog_title: 'タイトル', message: 'よろしく', comment: false)
+        user.save!
+      end
+
+      specify { user.receive_comment? ==  be_false }
     end
   end
 end
